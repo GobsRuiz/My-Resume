294a47992750e7be00bb3c23d14ab16ba2efe47b
Gabriel Ruiz <gabrielruiztq@gmail.com>
2021-01-22 18:01:54 -0300

    Working on responsiveness
    Update header

--- js/header/responsiveHeader.js
+++ js/header/responsiveHeader.js
@@ -47,11 +47,11 @@ function headerHamburguer_menu(animation){
 }
 
 function headerHamburguer_title(animation) {
-    if(animation == "open"){
-        headerLogo_h1.style.fontSize = "1.5vh";
-    }else{
-        headerLogo_h1.style.fontSize = "var(--headerLogoh1_fontSize)";
-    }
+    // if(animation == "open"){
+    //     headerLogo_h1.style.fontSize = "1.5vh";
+    // }else{
+    //     headerLogo_h1.style.fontSize = "var(--headerLogo_fontSize)";
+    // }
 }
 
 // Event listener - click
